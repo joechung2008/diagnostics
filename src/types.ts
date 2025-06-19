6b313f0d8261edff10afe3baa9f92d91b0347977
Joe Chung <joechung@microsoft.com>
2025-06-19 18:58:13 -0400

    Migrate to Fluent UI 9 (2)

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
-import { INavLink } from '@fluentui/react';
-
-export type KeyedNavLink = INavLink & { key: string };
+// Remove Fluent UI 8 dependency and define KeyedNavLink locally
+export type KeyedNavLink = {
+  key: string;
+  name: string;
+  url?: string;
+  [prop: string]: any;
+};
