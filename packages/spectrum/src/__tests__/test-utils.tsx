f67c1d80eb4a4e8a9ea9f628822824efcee55ea6
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-18 05:26:01 -0400

    Fix build break due to type definition mismatches

--- packages/spectrum/src/__tests__/test-utils.tsx
+++ packages/spectrum/src/__tests__/test-utils.tsx
@@ -4,7 +4,8 @@ import { render as testingLibraryRender } from "@testing-library/react";
 export function render(ui: React.ReactNode) {
   return testingLibraryRender(<>{ui}</>, {
     wrapper: ({ children }: { children: React.ReactNode }) => (
-      <Provider theme={defaultTheme}>{children}</Provider>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      <Provider theme={defaultTheme}>{children as any}</Provider>
     ),
   });
 }
