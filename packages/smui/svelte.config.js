6d176a8dbac5b90d103a1c2f46047ce66dd59f86
Joe Chung <joechung2008@users.noreply.github.com>
2025-09-29 03:47:43 -0400

    Add Dockerfile for smui; use @sveltejs/adapter-node

--- packages/smui/svelte.config.js
+++ packages/smui/svelte.config.js
@@ -1,15 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
+// Consult https://svelte.dev/docs/kit/integrations
+// for more information about preprocessors
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// adapter-node is suitable for Node.js SSR deployments.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
 	}
