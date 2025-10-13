77a9706d04613b5c44321eef4b2d1ec6e9b43f99
Joe Chung <26208574+joechung2008@users.noreply.github.com>
2025-10-13 12:58:56 -0400

    Fix lint configuration and update outdated packages in naive

---
+++ packages/naive/CHANGELOG.md
@@ -0,0 +1,29 @@
+# CHANGELOG
+
+## 2025-10-13
+
+- Fix lint configuration and update outdated packages
+
+## 2025-10-01
+
+- Deploy SvelteKit and Nuxt sites to Render
+
+## 2025-09-29
+
+- Add Dockerfile
+
+## 2025-09-28
+
+- Hoist @vue/compiler-dom and update outdated packages
+- Remove no longer used package-lock.json files
+
+## 2025-09-19
+
+- Revert "Turn off shared pnpm-lock.yaml"
+- Turn off shared pnpm-lock.yaml
+
+## 2025-09-18
+
+- Add diagnostics-naive as naive
+- Fix lint config and error
+- Remove jsdom as explicit dev dependency
--- packages/naive/eslint.config.mjs
+++ packages/naive/eslint.config.mjs
@@ -1,13 +1,23 @@
 // @ts-check
+import { parser as tsParser } from "typescript-eslint"
+import vueParser from "vue-eslint-parser"
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-export default withNuxt({
-  languageOptions: {
-    parserOptions: {
-      parser: "@typescript-eslint/parser"
+export default withNuxt(
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 2022,
+        sourceType: "module"
+      }
     }
   },
-  rules: {
-    "vue/multi-word-component-names": "off"
+  {
+    rules: {
+      "vue/multi-word-component-names": "off"
+    }
   }
-})
+)
